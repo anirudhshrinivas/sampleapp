7915192115dcf32d3339b3879b9777db5ff9dbda
anirudh <anirudhshrinivas@hotmail.com>
2014-06-11 01:26:57 +0530

    modified Pages Controller again

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get homecontact" do
-    get :homecontact
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
     assert_response :success
   end
 
